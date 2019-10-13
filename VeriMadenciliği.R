4e58ac3a37931bb81f19a8c225a494f37dcd0af5
GOKHAN KARA <GOKHANK@toyota.com.tr>
2019-10-13 15:26:17 +0300

    updated

--- VeriMadenciliği.R
+++ VeriMadenciliği.R
@@ -65,6 +65,8 @@ qt(0.025,100)
 
 
 # Independent t test (Equal variances assumed)
+# Birbirinden bağımsız iki ana kütle varsa;
+# Örnek, iki farklı grup olması gerekir. 2 farklı 9. sınıf öğrencilerine sınav yapmak gibi;
 library(car)
 library(carData)
 x3<-factor(x3)
@@ -75,8 +77,77 @@ t.test(x6~x3,var.equal=TRUE)
 leveneTest(x19~x3) 
 t.test(x19~x3,var.equal=TRUE)
 
+# Independent t test (Not Equal variances assumed)
+t.test(x19~x3)
+t.test(x6~x3, alternative='less',mu=0.8)
+t.test(x6~x3, alternative='greater',mu=0.2)
 
 
 
+
+
+
+
+
+
+
+
+
+
+# Paired t test
+t.test(x6~x3,Paired = TRUE)
+
+
+# independent 2-group Mann-Whitney U Test (Nonparametric test)
+# y metrik, x ikili değişken
+wilcox.test(x6~x3) 
+
+# Nonparametric Paired Test
+wilcox.test(x6,x7, paired=TRUE) # x ve y metrik 
+
+
+
+
+# Anova
+x1<-factor(x1)
+boxplot(x19~x1,col=rainbow(7))
+model<-aov(x19~x1)
+summary(model)
+leveneTest(x19~x1)
+library(PMCMRplus)
+# Varyanslar ayni degil ise Tamhane test
+tamhaneT2Test(x19~x1)
+# Varyanslar ayni ise Tukey
+TukeyHSD(model)
+library(mvtnorm)
+library(DunnettTests)
+qf(0.95,2,97)
+dunnettTest(x19~x1)
+
+
+# Two way Anova
+x4<-factor(x4)
+model<-aov(x19~x1*x4)
+summary(model)
+TukeyHSD(model)
+table(x1,x4)
+
+#Correlation
+install.packages("corrplot")
+library(corrplot)
+HBAT1<-HBAT[ ,7:19]
+corrplot(cor(HBAT1), order = "hclust", tl.col='black', tl.cex=.75) 
+corrplot(cor(HBAT1), order = "hclust") 
+corrplot(cor(HBAT1), method = "ellipse")
+library(car)
+attach(HBAT)
+scatterplot(x18,x9)
+
+cor(x18,x9)
+cor.test(x18,x9)
+cor.test(x15,x18)
+cor.test(x10,x14)
+cor(x10,x14)
+cor.test(x10,x14)
 
 
